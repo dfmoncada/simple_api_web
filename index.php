9b2fec586cab98c136e55a1be0b6908e2ecd14bf
diego <dfmoncada@gmail.com>
2018-06-29 03:16:21 +0000

    added 401 header

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
 if(!isset($_GET['api-key']) || $_GET['api-key'] != 'maiAPIkey'){
-    echo json_encode(["response"=>"FAILED", "message"=>"the API key you entered was not correct"]);
+        echo json_encode(["response"=>"FAILED", "message"=>"the API key you entered was not correct"]);
+        header("HTTP/1.1 401 Unauthorized");
     return;
 }
 
